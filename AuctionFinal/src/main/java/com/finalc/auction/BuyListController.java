7e2c176531e7adb5246695fe084fb0964b26ab63
user1 <user1@DESKTOP-BIBKLL9>
2018-07-03 19:38:03 +0900

    11

--- AuctionFinal/src/main/java/com/finalc/auction/BuyListController.java
+++ AuctionFinal/src/main/java/com/finalc/auction/BuyListController.java
@@ -31,13 +31,20 @@ public class BuyListController {
 		System.out.println("0. loginuser : " + loginuser);
 		session.setAttribute("loginuser", loginuser);
 		System.out.println("1. loginuser.userid : " + loginuser.getUserid());
-		
 		List<HashMap<String, String>> buyMapList = new ArrayList<HashMap<String, String>>();
-		buyMapList = service.getBuyList(loginuser.getUsernum());
-		System.out.println("?. Controller 단 buyMapList : " + buyMapList);
+		if (loginuser != null) {
+			
+			buyMapList = service.getBuyList(loginuser.getUsernum());
+			System.out.println(". Controller 단 buyMapList : " + buyMapList);
+			req.setAttribute("buyMapList", buyMapList);
+			return "buy/buyList.tiles";
+		}
+		else {
+			req.setAttribute("msg", "로그인을 먼저 하십시오!");
+			req.setAttribute("loc", "javascript:history.back();");
+			return "msg.notiles";
+		}
 		
-		req.setAttribute("buyMapList", buyMapList);
 		
-		return "buy/buyList.tiles";
 	}
 }
