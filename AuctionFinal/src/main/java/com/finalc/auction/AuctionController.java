f6e0bf7833b3166a810baaa2923dc6027a53c5ff
user1 <user1@DESKTOP-BIBKLL9>
2018-07-05 12:20:05 +0900

    ㅁㄴㅇㄹ

--- AuctionFinal/src/main/java/com/finalc/auction/AuctionController.java
+++ AuctionFinal/src/main/java/com/finalc/auction/AuctionController.java
@@ -1,6 +1,7 @@
 package com.finalc.auction;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -31,9 +32,7 @@ public class AuctionController {
 	
 	// 옥션 상품 게시 기능 (0704)
 	@RequestMapping(value="/AuctionUpload.action", method={RequestMethod.GET})  
-	public String before_AuctionUpload(HttpServletRequest req) {
-		
-		
+	public String auctionLogin_AuctionUpload(HttpServletRequest req, HttpServletResponse res) {
 		
 	return "auction/AddAuction.tiles";
 	} // 옥션 상품 게시 기능
