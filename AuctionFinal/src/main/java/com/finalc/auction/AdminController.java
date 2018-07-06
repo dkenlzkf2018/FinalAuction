f8243a65c88f82670f72f3a416b469f740678427
user1 <user1@DESKTOP-BIBKLL9>
2018-07-06 16:07:33 +0900

    관리자페이지 추가 0706 3:53 push

--- AuctionFinal/src/main/java/com/finalc/auction/AdminController.java
+++ AuctionFinal/src/main/java/com/finalc/auction/AdminController.java
@@ -2,6 +2,8 @@ package com.finalc.auction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.finalc.auction.service.InterAdminService;
 
