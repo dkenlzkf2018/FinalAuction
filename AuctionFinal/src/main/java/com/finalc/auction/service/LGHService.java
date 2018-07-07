831f966d44775ebd18ac210adce9ca4301c4fd3d
user1 <user1@DESKTOP-BIBKLL9>
2018-07-07 17:27:05 +0900

    memberList
    개꿀 멤버리스트

---
+++ AuctionFinal/src/main/java/com/finalc/auction/service/LGHService.java
@@ -0,0 +1,21 @@
+package com.finalc.auction.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.finalc.auction.model.InterLGHDAO;
+import com.finalc.auction.model.MemberVO;
+
+@Service
+public class LGHService implements InterLGHService {
+	@Autowired
+	private InterLGHDAO dao;
+
+	@Override
+	public List<MemberVO> getMemberList() {
+		List<MemberVO> memberList = dao.getMemberList();
+		return memberList;
+	}
+}
